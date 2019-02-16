07a9f1be62872a3c735716eb8ada22cef59bbfc3
Tanner Gill <contacttannergill@gmail.com>
2019-02-16 10:44:43 -0800

    Implemented the person interface

--- greeter.js
+++ greeter.js
@@ -1,5 +1,11 @@
+// We use the interface name just as you would annotate a type, like we would with "string", except
+// rather than being a lowercase reference it is pascal cased; denoting it is an interface.
 function hello(person) {
-    return "Hello, " + person;
+    return "Hello, " + person.firstName + " " + person.lastName;
 }
-var myPerson = "Tanner";
+// Now, the data we're passing to the "hello" function must match the Person interface.
+var myPerson = {
+    firstName: "Tanner",
+    lastName: "Gill"
+};
 document.body.innerHTML = hello(myPerson);
--- greeter.ts
+++ greeter.ts
@@ -1,7 +1,21 @@
-function hello(person: string): string {
-  return "Hello, " + person;
+// An interface is similar to a database schema, 
+// enforcing that specific keys should have specific datatypes as values.
+interface Person {
+  // In this case, the firstName and lastName keys must have strings as their value.
+  firstName: string
+  lastName: string
 }
 
-var myPerson = "Tanner";
+// We use the interface name just as you would annotate a type, like we would with "string", except
+// rather than being a lowercase reference it is pascal cased; denoting it is an interface.
+function hello(person: Person): string {
+  return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+// Now, the data we're passing to the "hello" function must match the Person interface.
+var myPerson = {
+  firstName: "Tanner",
+  lastName: "Gill"
+};
 
 document.body.innerHTML = hello(myPerson);
