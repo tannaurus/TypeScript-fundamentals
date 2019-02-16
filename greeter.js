ac023c66685baa8e1eddf2c98f1094ffe79b4281
Tanner Gill <contacttannergill@gmail.com>
2019-02-16 11:01:49 -0800

    Implemented a class, learning access modifiers and the class to interface relationship

--- greeter.js
+++ greeter.js
@@ -1,11 +1,23 @@
+var Student = /** @class */ (function () {
+    // The access modifiers here (private & public) do not effect the final JS code,
+    // but allow us to be that much more stricter when defining our class.
+    // - Private values can only be accessed from within the class
+    // - Public values can be access from both inside and outside the class, the latter using dot notation.
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
 // We use the interface name just as you would annotate a type, like we would with "string", except
 // rather than being a lowercase reference it is pascal cased; denoting it is an interface.
 function hello(person) {
     return "Hello, " + person.firstName + " " + person.lastName;
 }
-// Now, the data we're passing to the "hello" function must match the Person interface.
-var myPerson = {
-    firstName: "Tanner",
-    lastName: "Gill"
-};
+// We will give the "hello" function a new instance of the Student class, which
+// meets the requirements of the Person interface; since both the firstName and lastName
+// properties are public and can be accessed through dot notation, just like Objects.
+var myPerson = new Student("Tanner", "M", "Gill");
 document.body.innerHTML = hello(myPerson);
--- greeter.ts
+++ greeter.ts
@@ -1,3 +1,14 @@
+class Student {
+  // The access modifiers here (private & public) do not effect the final JS code,
+  // but allow us to be that much more stricter when defining our class.
+  // - Private values can only be accessed from within the class
+  // - Public values can be access from both inside and outside the class, the latter using dot notation.
+  constructor(public firstName: string, private middleInitial: string, public lastName: string) {
+    this.fullName = `${firstName} ${middleInitial} ${lastName}`;
+  }
+  fullName: string;
+}
+
 // An interface is similar to a database schema, 
 // enforcing that specific keys should have specific datatypes as values.
 interface Person {
@@ -9,13 +20,12 @@ interface Person {
 // We use the interface name just as you would annotate a type, like we would with "string", except
 // rather than being a lowercase reference it is pascal cased; denoting it is an interface.
 function hello(person: Person): string {
-  return "Hello, " + person.firstName + " " + person.lastName;
+  return `Hello, ${person.firstName} ${person.lastName}`;
 }
 
-// Now, the data we're passing to the "hello" function must match the Person interface.
-var myPerson = {
-  firstName: "Tanner",
-  lastName: "Gill"
-};
+// We will give the "hello" function a new instance of the Student class, which
+// meets the requirements of the Person interface; since both the firstName and lastName
+// properties are public and can be accessed through dot notation, just like Objects.
+var myPerson = new Student("Tanner", "M", "Gill");
 
 document.body.innerHTML = hello(myPerson);
